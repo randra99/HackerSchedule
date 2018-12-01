1913712d777f4609b3308f16bf47a000f0082d83
Radu Andra <radu-tudor.andra@student.manchester.ac.uk>
2018-12-01 13:08:59 +0000

    Added Camera functionality

--- app/src/main/java/com/example/wildcats/MainActivity.java
+++ app/src/main/java/com/example/wildcats/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.wildcats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button openCbt = (Button) findViewById(R.id.camerabt);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openCbt.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, CameraAcitivity.class));
+            }
+        });
     }
 }
