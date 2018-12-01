e35852aceefa18dba58b9d6cbbde33bab09df196
Vlad Ianculescu <vlad.ianculescu@student.manchester.ac.uk>
2018-12-01 13:10:11 +0000

    Part1

---
+++ app/src/main/java/com/example/wildcats/ParseJSON.java
@@ -0,0 +1,20 @@
+package com.example.wildcats;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+public class ParseJSON extends AppCompatActivity{
+    public ParseJSON()
+    {
+
+    }
+
+    public void test()
+    {
+        System.out.println("asdasd");
+    }
+}
