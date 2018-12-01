7adb37fe528114dee030c442366c907a968938df
Radu Andra <radu-tudor.andra@student.manchester.ac.uk>
2018-12-01 17:28:41 +0000

    Camera fully working!

--- app/src/main/java/com/example/wildcats/CameraAcitivity.java
+++ app/src/main/java/com/example/wildcats/CameraAcitivity.java
@@ -1,54 +1,22 @@
 package com.example.wildcats;
 
-
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.graphics.ImageFormat;
-import android.graphics.SurfaceTexture;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.CameraMetadata;
-import android.hardware.camera2.CaptureRequest;
-import android.hardware.camera2.TotalCaptureResult;
-import android.hardware.camera2.params.StreamConfigurationMap;
-import android.media.Image;
-import android.media.ImageReader;
 import android.os.Environment;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Size;
-import android.util.SparseIntArray;
-import android.view.Surface;
-import android.view.TextureView;
+
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.camerakit.CameraKitView;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
 
 public class CameraAcitivity extends AppCompatActivity {
 
     private CameraKitView cameraKitView;
     private Button photoButton;
+    public String storedfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,6 +66,8 @@ public class CameraAcitivity extends AppCompatActivity {
                         FileOutputStream outputStream = new FileOutputStream(savedPhoto.getPath());
                         outputStream.write(capturedImage);
                         outputStream.close();
+                        storedfile = savedPhoto.getPath();
+                        System.out.println(storedfile);
                     } catch (java.io.IOException e) {
                         e.printStackTrace();
                     }
